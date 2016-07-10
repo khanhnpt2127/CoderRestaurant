a0ddb03e32a8ad2ec40da87fb1fd680bf3464715
khanhnpt2127 <khanh.npt2127@hotmail.com.vn>
2016-07-10 17:04:39 +0700

    Welcome Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'contact_us' => 'home#index'
+  get 'menu' => 'home#index'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
